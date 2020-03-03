e5a81d74136e4da8301a019567391271c21dfc16
David Honegger Rogers <dhr@lanl.gov>
2020-03-03 14:37:21 -0700

    updating test

--- testing/test_cis.py
+++ testing/test_cis.py
@@ -8,58 +8,67 @@ class TestCIS(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestCIS, self).__init__(*args, **kwargs)
 
-        self.result_dir  = "testing"
-        self.result_file = "composable.cis"
+        self.result_dir  = 'testing'
+        self.result_file = 'composable.cis'
         self.result_fullpath = os.path.join(self.result_dir, self.result_file)
 
     def test_create_hdf5(self):
-
-        channels = ["depth", "lighting", "temperature", "pressure", "procID"]
-
         myCIS = cinemagic.cis.cis(self.result_fullpath)
         myCIS.set_size(1024, 768)
 
-        ptable = pandas.read_csv("testing/data/example.csv", dtype=str, keep_default_na=False) 
+        ptable = pandas.read_csv('testing/data/example.csv', dtype=str, keep_default_na=False)
         myCIS.set_parameter_table(ptable)
 
-        myCIS.add_variable("temperature", "float", 0.0, 1000.0)
-        myCIS.add_variable("pressure", "float", 0.50, 5000.0)
-        myCIS.add_variable("procID", "int", 0, 1024) 
+        variables = [ ['temperature', 10.0, 100.0],
+                      ['pressure',    20.0, 200.0],
+                      ['procID',      30.0, 300.0]
+                    ]
+        for v in variables:
+            myCIS.add_variable(v[0], 'float', v[1], v[2])
 
-        myCIS.add_parameter("time", "float")
-        myCIS.add_parameter("phi", "float")
-        myCIS.add_parameter("theta", "float")
-        myCIS.add_parameter("isovar", "string")
-        myCIS.add_parameter("isoval", "float")
+        parameters = ['time', 'phi', 'theta', 'isovar', 'isoval']
+        for p in parameters:
+            myCIS.add_parameter(p, 'float')
 
-        myCIS.add_image("0000")
-
-        image = myCIS.get_image("0000")
-        layer = image.add_layer("l000")
-        layer.set_offset(60, 60)
-        layer.set_size(2, 2)
-        for c in channels: 
-            channel = layer.add_channel(c)
-
-        layer = image.add_layer("l001")
-        layer.set_offset(90, 90)
-        layer.set_size(4, 4)
-        for c in channels: 
-            channel = layer.add_channel(c)
-
-        layer = image.add_layer("l002")
-        layer.set_offset(110, 110)
-        layer.set_size(6, 6)
-        for c in channels: 
-            channel = layer.add_channel(c)
+        images = ['0000', '0001', '0002']
+        for i in images:
+            self.add_test_image(myCIS, i)
 
         # write to different storage formats 
         hdf5_writer = cinemagic.cis.write.hdf5.hdf5_writer()
         hdf5_writer.write(myCIS)
 
-        print(myCIS.get_image_names())
-
+        # print(myCIS.get_image_names())
         self.check_hdf5()
+
+    def add_test_image(self, cis, imName):
+        channels = ['depth', 'lighting', 'temperature', 'pressure', 'procID']
+
+        cis.add_image(imName)
+        image = cis.get_image(imName)
+
+        layerData = {
+            'l000' : {
+                'offset' : [0, 10],
+                'size'   : [10, 20]
+            },
+            'l001': {
+                'offset' : [100, 110],
+                'size'   : [30, 40]
+            },
+            'l002': {
+                'offset' : [200, 210],
+                'size'   : [50, 60]
+            }
+        }
+
+        for l in layerData:
+            layer = image.add_layer(l)
+            layer.set_offset( layerData[l]['offset'][0], layerData[l]['offset'][0] )
+            layer.set_size( layerData[l]['size'][0], layerData[l]['size'][0] )
+            for c in channels:
+                channel = layer.add_channel(c)
+                channel.create_test_data()
 
     def check_hdf5(self):
         assert os.path.exists(self.result_fullpath)
