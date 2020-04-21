83809581fae7ca2fd160b167a9389e0c13eb4dbf
David Honegger Rogers <dhr@lanl.gov>
2020-04-21 15:16:39 -0600

    updating render

--- cinemagic/cis/render.py
+++ cinemagic/cis/render.py
@@ -1,10 +1,20 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
 class render():
 
     def __init__(self):
         return
 
-    def render(self, cis):
-        return Image.new(mode="RGB", size=(cis.dims[0], cis.dims[1])) 
+    def render(self, cis, iname, lname):
+        image = cis.get_image(iname)
+
+        im = Image.new(mode="RGB", size=(cis.dims[0], cis.dims[1])) 
+        for layer in image.get_layers():
+            l = image.get_layer(layer)
+            shape = [(l.offset[0], l.offset[1]), 
+                    (l.offset[0] + l.dims[0], l.offset[1] + l.dims[1])]
+            imgfinal = ImageDraw.Draw(im)
+            imgfinal.rectangle(shape, fill="#ffff33", outline="red")
+
+        return im
 
