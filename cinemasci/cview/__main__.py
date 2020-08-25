1fbd51b9cc2575445d541ad274c97432c3d04911
David Honegger Rogers <dhr@lanl.gov>
2020-08-25 10:01:05 -0600

    updating to PV standard CLA for viewing

--- cinemasci/cview/__main__.py
+++ cinemasci/cview/__main__.py
@@ -1,31 +1,27 @@
 
-def main( cview_type, database ):
+def main( database, port, cview_type ):
     import cinemasci
     import http.server
     import socketserver
 
     localhost = "http://127.0.0.1"
-    PORT      = 8100
 
     viewer = "cinemasci/cview/{}/cinema_{}.html".format(cview_type, cview_type)
     database = database 
 
     Handler   = http.server.SimpleHTTPRequestHandler
-    with socketserver.TCPServer(("", PORT), Handler) as httpd:
-        print("{}:{}/{}?databases={}".format(localhost, PORT, viewer, database))
+    with socketserver.TCPServer(("", port), Handler) as httpd:
+        print("{}:{}/{}?databases={}".format(localhost, port, viewer, database))
         httpd.serve_forever()
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="run a Cinema Viewer")
-    parser.add_argument("database", help="database to view") 
-    # parser.add_argument("--viewer", default=None, help="viewer type") 
+    parser.add_argument("--data", default=None, help="database to view") 
+    parser.add_argument("--port", type=int, default=8000, help="port to use") 
+    parser.add_argument("--viewer", default='explorer', help="viewer type to use") 
     args = parser.parse_args()
 
-    #if args.viewer is None:
-        #args.viewer = "explorer"
-
     # currently only viewer type explorer is supported
-    main( "explorer", args.database) 
-
+    main( args.data, args.port, args.viewer) 
