cff2daa57058c99ab6e4b7d705c4ceb3ecf5ab2a
David Honegger Rogers <dhr@lanl.gov>
2020-08-26 10:32:25 -0600

    adding more comments

--- cinemasci/cview/__main__.py
+++ cinemasci/cview/__main__.py
@@ -5,17 +5,30 @@ from urllib.parse import urlparse
 from urllib.parse import parse_qs
 from os.path import relpath
 from os import getcwd
+from os import access
+from os import R_OK
 
+#
+# global datbase - can't seem to add an instance variable to the
+# subclass of SimpleHTTPRequestHandler
+#
 TheDatabase = "CINEMAJUNK" 
+CinemaInstallPath = "cinemasci/cview"
 
+#
+# CinemaReqestHandler
+#
+# Processes GET requests to find viewers and databases
+#
 class CinemaRequestHandler(http.server.SimpleHTTPRequestHandler):
 
     def log(self, message):
-        if False:
+        if True:
             print(message)
 
     def do_GET(self):
         global TheDatabase
+        global CinemaInstallPath
 
         self.log("PATH ORIG: {}".format(self.path))
         query_components = parse_qs(urlparse(self.path).query)
@@ -23,6 +36,7 @@ class CinemaRequestHandler(http.server.SimpleHTTPRequestHandler):
         self.path = self.path.split("?")[0]
         self.log("PATH     : {}".format(self.path))
 
+        # set attributes from a query in the GET URL
         if "databases" in query_components:
             TheDatabase = query_components["databases"][0]
             if not TheDatabase.startswith("/"):
@@ -30,29 +44,44 @@ class CinemaRequestHandler(http.server.SimpleHTTPRequestHandler):
             self.log("SET DB   : {}".format(TheDatabase))
 
         if self.path.startswith(TheDatabase):
-            self.log("DB QUERY : {}".format(self.path))
-            self.path = relpath(self.path, getcwd())
-            self.log("CWD      : {}".format(getcwd()))
-            self.log("REL DB   : {}".format(self.path))
-            return http.server.SimpleHTTPRequestHandler.do_GET(self)
+            # handle requests to the database
+
+            # remap absolute paths
+            if TheDatabase.startswith("/"):
+                self.log("DB QUERY : {}".format(self.path))
+                self.path = relpath(self.path, getcwd())
+                self.log("CWD      : {}".format(getcwd()))
+                self.log("REL DB   : {}".format(self.path))
+
+            if access(self.path, R_OK):
+                self.log("ACCESSING: {}".format(self.path))
+                return http.server.SimpleHTTPRequestHandler.do_GET(self)
+            else:
+                print("ERROR: cannot access file: {}".format(self.path))
 
         elif self.path == "/explorer":
+            # handle a request for the Cinema:Explorer viewer
             self.log("EXPLORER")
-            self.path = "cinemasci/cview/cinema_explorer.html" 
+            self.path = CinemaInstallPath + "/cinema_explorer.html" 
             return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
         elif self.path == "/view":
+            # handle a request for the Cinema:View viewer
             self.log("VIEW")
-            self.path = "cinemasci/cview/cinema_view.html" 
+            self.path = CinemaInstallPath + "/cinema_view.html" 
             return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
         elif self.path.startswith("/cinema"):
+            # handle a requests for sub components of the viewers 
+            # NOTE: fragile - requires 'cinema' path be unique
+
             self.log("CINEMA   : {}".format(self.path))
-            self.path = "cinemasci/cview" + self.path
+            self.path = CinemaInstallPath + self.path
             self.log("        {}".format(self.path))
             return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
         else:
+            # everything else
             self.log("NORMAL   : {}".format(self.path))
             return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
