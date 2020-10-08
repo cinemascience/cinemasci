99a9446bf476f55bb1272f40837afbb8d819c0a8
David Honegger Rogers <dhr@lanl.gov>
2020-10-08 13:09:53 -0600

    updates to server and install

--- cinemasci/server/__init__.py
+++ cinemasci/server/__init__.py
@@ -2,6 +2,7 @@ import http.server
 import socketserver
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
+from os import listdir
 from os import path
 from os.path import relpath
 from os import getcwd
@@ -15,10 +16,13 @@ import pathlib
 #
 TheDatabase = "CINEMAJUNK" 
 CinemaInstallPath = "CINEMAJUNK"
+ServerInstallPath = "CINEMAJUNK"
 
 def set_install_path():
     global CinemaInstallPath
+    global ServerInstallPath
 
+    ServerInstallPath = str(pathlib.Path(__file__).parent.absolute())
     CinemaInstallPath = str(pathlib.Path(__file__).parent.absolute())
     # edit the path to get the correct installation path
     CinemaInstallPath = CinemaInstallPath.strip("/server")
@@ -26,7 +30,7 @@ def set_install_path():
     print("CinemaInstallPath: {}".format(CinemaInstallPath))
 
 def get_relative_install_path( initpath ):
-    global CinemaInstallPath 
+    global CinemaInstallPath
 
     result = path.join(CinemaInstallPath, initpath.strip("/"))
     result = relpath(result, getcwd())
@@ -40,25 +44,34 @@ def get_relative_install_path( initpath ):
 #
 class CinemaRequestHandler(http.server.SimpleHTTPRequestHandler):
 
+    def translate_path(self, path ):
+        print("translate_path: {}".format(path))
+        return path 
+
     def log(self, message):
-        if True:
+        if False:
             print(message)
 
     def do_GET(self):
         global TheDatabase
+        global CinemaInstallPath
+        global ServerInstallPath
 
         self.log("PATH ORIG: {}".format(self.path))
         query_components = parse_qs(urlparse(self.path).query)
         self.log("QUERY    : {}".format(query_components))
         self.path = self.path.split("?")[0]
         self.log("PATH     : {}".format(self.path))
+        self.log("DBPATH   : {}".format(TheDatabase))
 
         # set attributes from a query in the GET URL
         if "databases" in query_components:
             TheDatabase = query_components["databases"][0]
-            # if not TheDatabase.startswith("/"):
-                # TheDatabase = "/" + TheDatabase
             self.log("SET DB   : {}".format(TheDatabase))
+
+            if not path.isdir(TheDatabase):
+                self.path = ServerInstallPath + "/error_no-database.html"
+                return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
         if "viewer" in query_components: 
             # handle a request for a viewer 
@@ -66,27 +79,37 @@ class CinemaRequestHandler(http.server.SimpleHTTPRequestHandler):
             if viewer == "explorer": 
                 # handle a request for the Cinema:Explorer viewer
                 self.log("EXPLORER")
-                self.path = get_relative_install_path("../viewers/cinema_explorer.html")
+                self.path = CinemaInstallPath + "/cinema_explorer.html"
                 return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
             elif viewer == "view": 
                 # handle a request for the Cinema:View viewer
                 self.log("VIEW")
-                self.path = get_relative_install_path("../viewers/cinema_view.html")
+                self.path = CinemaInstallPath + "/cinema_view.html"
+                return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+            if viewer == "test": 
+                # handle a request for the Cinema:Explorer viewer
+                self.log("TEST")
+                self.path = ServerInstallPath + "/cinema_test.html"
                 return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
             else:
                 self.log("VIEWER: -{}-".format(viewer))
 
-        if self.path.startswith(TheDatabase):
+        if self.path.startswith("/" + TheDatabase):
             # handle requests to the database
 
             # remap absolute paths
-            if TheDatabase.startswith("/"):
-                self.log("DB QUERY : {}".format(self.path))
-                self.path = relpath(self.path, getcwd())
-                self.log("CWD      : {}".format(getcwd()))
-                self.log("REL DB   : {}".format(self.path))
+            if False:
+                self.log("DATABASE QUERY : {}".format(self.path))
+                if TheDatabase.startswith("/"):
+                    self.log("DB QUERY : {}".format(self.path))
+                    self.path = relpath(self.path, getcwd())
+                    self.log("CWD      : {}".format(getcwd()))
+                    self.log("REL DB   : {}".format(self.path))
+
+            self.path = self.path.strip("/")
 
             if access(self.path, R_OK):
                 self.log("ACCESSING: {}".format(self.path))
@@ -99,7 +122,7 @@ class CinemaRequestHandler(http.server.SimpleHTTPRequestHandler):
             # NOTE: fragile - requires 'cinema' path be unique
 
             self.log("CINEMA   : {}".format(self.path))
-            self.path = get_relative_install_path(self.path)
+            self.path = CinemaInstallPath + self.path 
             self.log("        {}".format(self.path))
             return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
