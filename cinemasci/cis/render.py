55a689501a93d193f8942e296d9f85ed7600c327
Li-Ta Lo <ollie@lanl.gov>
2021-02-22 16:42:44 -0700

    color with colormap

--- cinemasci/cis/render.py
+++ cinemasci/cis/render.py
@@ -1,5 +1,6 @@
 import numpy as np
 import skimage.color
+from scipy.interpolate import interp1d
 
 
 def paste(x, y, offset):
@@ -24,12 +25,17 @@ class render():
         image = cis.get_image(iname)
 
         result = np.zeros((cis.dims[1], cis.dims[0], 3))
+        points = cis.colormaps['blue-1'].get_points()
+        colormap = np.asarray(points)
 
+        values = colormap[:, 0]
+        rgbs = colormap[:, 2:]
+        cmap = interp1d(values, rgbs, axis=0)
         # for now, assume only variables (no shadow, depth, etc.)
         for layer in lnames:
             l = image.get_layer(layer)
             if l:
-                arr = skimage.color.gray2rgb(l.get_channel(cnames[0]).data)
+                arr = cmap(l.get_channel(cnames[0]).data)
                 result = paste(result, arr, l.offset)
 
         return result
