0a6b7b8c2ca322c2ac1ae199e22a455a7ab1db9f
David Honegger Rogers <dhr@lanl.gov>
2021-05-20 13:16:41 -0600

    added comment pointing out failure

--- testing/test_cis.py
+++ testing/test_cis.py
@@ -237,6 +237,7 @@ class TestCIS(unittest.TestCase):
 
         # set the imageview state
         iview.image = "cycle_000100"
+            # CHANGE TO FALSE, and the test fails in the render
         iview.use_depth = True
         iview.use_shadow = False
         iview.activate_layer("layer0")
