321ae2966d15389b424abea4cd9abd55bfce9ce5
David Honegger Rogers <dhr@lanl.gov>
2021-06-16 16:33:45 -0600

    adding size controls

--- cinemasci/pynb/cis.py
+++ cinemasci/pynb/cis.py
@@ -28,9 +28,28 @@ class ParamSliders():
 class CISViewer():
 
     def __init__(self, cdbview):
+        self.aspect  = "equal"
+        self.size    = (10, 10)
         self.cdbview = cdbview
         self.sliders = ParamSliders(cdbview.get_cdb_parameters())
         return
+
+    @property
+    def aspect(self):
+        return self._aspect
+
+    @aspect.setter
+    def aspect(self, value):
+        self._aspect = value
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        self._size = value
+
 
     def display(self, image):
         import matplotlib.pyplot as plt
@@ -40,8 +59,9 @@ class CISViewer():
             warnings.simplefilter("ignore")
             # plt.figure(figsize=(1,1))
             # plt.imshow(skimage.util.img_as_ubyte(image))
-            plt.imshow(image)
+            plt.figure(figsize = self.size) 
             plt.axis('off')
-            plt.show()
+            plt.imshow(image, aspect=self.aspect)
+            # plt.show()
 
 
