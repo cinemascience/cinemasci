8444ea064ce9fae61a5c012b6841ac9cedc767c8
Li-Ta Lo <ollie@lanl.gov>
2021-06-16 16:09:28 -0600

    Correct background color for use_depth = false.

--- cinemasci/cis/renderer.py
+++ cinemasci/cis/renderer.py
@@ -62,23 +62,29 @@ class Renderer:
     def render(iview):
 
         # FXIME: this assumes RGB rather than RGBA color
-        canvas = np.full((iview.dims[0], iview.dims[1], 3), iview.background, float)
+        canvas = np.full((iview.dims[0], iview.dims[1], 3), iview.background,
+                         float)
         depth = np.full((iview.dims[0], iview.dims[1]), np.inf, float)
 
         # TODO: how to make use of 'origin'?
         layers = iview.get_layer_data()
         for name, layer in layers.items():
             data = layer.channel.data
-            colored = Renderer.color(data, layer.channel.colormap)
+            background = np.full((data.shape[0], data.shape[1], 3),
+                                 iview.background, float)
+            foreground = Renderer.color(data, layer.channel.colormap)
+            Renderer.blend(foreground, background, np.isnan(data))
+
             rectangle = [
                 slice(layer.offset[0], layer.offset[0] + data.shape[0]),
                 slice(layer.offset[1], layer.offset[1] + data.shape[1])]
             if iview.use_depth:
                 Renderer.depth_composite(canvas[tuple(rectangle)],
                                          depth[tuple(rectangle)],
-                                         colored, layer.depth.data)
+                                         foreground, layer.depth.data)
             else:
-                canvas = Renderer.paste(canvas, colored, layer.offset)
+                canvas = Renderer.paste(canvas, foreground, layer.offset)
+
         # iview.dims is in row by column rather than x-dims by y-dims,
         # we need to transpose the two axes.
         return canvas.transpose((1, 0, 2)), depth.T
