7b763706080418829d83f8cc97690b7c7be6300e
David Honegger Rogers <dhr@lanl.gov>
2021-06-24 10:07:02 -0600

    updates for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="cinemasci",
-    version="1.4",
+    version="1.5",
     author="David H. Rogers",
     author_email="dhr@lanl.gov",
     description="Cinema scientific toolset.",
