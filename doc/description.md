e02f7565a95c60c9aacca6c514e05cfb0dc5a32f
David Honegger Rogers <dhr@lanl.gov>
2021-06-24 10:22:15 -0600

    update

---
+++ doc/description.md
@@ -0,0 +1,3 @@
+# Cinema python tools
+
+The `cinemasci` python module is a set of tools for reading, writing, manipulating and viewing Cinema databases. More about Cinema can be found at http://cinemascience.org.
--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import setuptools
 
+# read the description file 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'doc/description.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="cinemasci",
     version="1.5",
     author="David H. Rogers",
     author_email="dhr@lanl.gov",
     description="Cinema scientific toolset.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/cinemascience",
     include_package_data=True,
     packages=["cinemasci", 
