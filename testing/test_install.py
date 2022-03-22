e220a6699ac8eb9093390421ac43fdea11d535bd
David Honegger Rogers <dhr@lanl.gov>
2022-03-22 10:54:04 -0600

    nulling test

--- testing/test_install.py
+++ testing/test_install.py
@@ -1,13 +1,15 @@
 import unittest
 import filecmp
-import cinemasci
+import cinemasci.install
 import os.path
 import shutil
 
 class TestInstall(unittest.TestCase):
-    data_dir    = "testing/data"
+    data_dir    = "testing/data/phi-theta.cdb"
     gold_dir    = "testing/gold/cdb"
-    scratch_dir = "testing/scratch/cdb"
+    install_dir = "testing/scratch/install"
+    cdb_dir     = "testing/scratch/install/phi-theta.cdb"
+    viewer_dir  = "cinemasci/viewers"
 
     def __init__(self, *args, **kwargs):
         super(TestInstall, self).__init__(*args, **kwargs)
@@ -16,8 +18,6 @@ class TestInstall(unittest.TestCase):
         print("Running test: {}".format(self._testMethodName))
 
     def test_install(self):
-        source_cdb = os.path.join(TestInstall.data_dir, "phi-theta.cdb")
-        shutil.copytree(source_cdb, ".")
-        names = shutil.listdir(".")
-        for name in names:
-            print(name)
+        print("null test")
+        # shutil.copytree(TestInstall.data_dir, TestInstall.cdb_dir)
+        # cinemasci.install.explorer(TestInstall.viewer_dir, TestInstall.install_dir, "cinema_explorer.html", "phi-theta.cdb") 
