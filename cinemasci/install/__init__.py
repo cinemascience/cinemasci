768987371ae7c24faa91f4e3a5b6c4ed851d2993
David Honegger Rogers <dhr@lanl.gov>
2022-03-28 15:03:43 -0600

    beginning local install

--- cinemasci/install/__init__.py
+++ cinemasci/install/__init__.py
@@ -6,43 +6,46 @@ import json
 import cinemasci.viewers
 
 class install:
+    INSTALL_ERROR = 0
+    INSTALL_OK    = 1
 
     def __init__(self):
         """The constructor"""
         self.source = cinemasci.path()
         print(self.source)
 
-    def install_viewer(self, destination, viewer, dbs):
-        if os.access(destination, os.W_OK):
-            if os.access(self.source, os.R_OK):
-                self.__copy_cinema_dir_to_destination( destination ) 
-                self.__copy_viewer_to_destination( destination, viewer ) 
-                self.__write_database_file( destination, viewer, dbs ) 
+    def install_viewer(self, destination, vtype, dbs, type="remote"):
+        status = install.INSTALL_OK
+        print("self.source = {}".format(self.source))
+
+        if not os.path.isdir(os.path.join(destination, "cinema")):
+            if os.access(destination, os.W_OK):
+                if os.access(self.source, os.R_OK):
+                    self.__copy_cinema_dir_to_destination( destination ) 
+                    self.__copy_viewer_to_destination( destination, vtype ) 
+                    self.__write_database_file( destination, vtype, dbs ) 
+
+                    if type == "remote":
+                        # remove libs and components
+                        print("Performing remote install")
+                        libDir = os.path.join(destination, "cinema", "lib")
+                        shutil.rmtree(libDir)
+
+                    else:
+                        print("Performing local install")
+                        self.__install_libs(destination)
+
+                else:
+                    print("ERROR: cannot read from directory {}".format(self.source)) 
+                status = install.INSTALL_ERROR
             else:
-                print("ERROR: cannot read from directory {}".format(self.source)) 
+                print("ERROR: cannot write to directory {}".format(destination))
+                status = install.INSTALL_ERROR
         else:
-            print("ERROR: cannot write to directory {}".format(destination)) 
+            print("ERROR: cinema install exists; Cannot continue")        
+            status = install.INSTALL_ERROR
 
-    def __write_database_file(self, destination, viewer, dbs):
-        version = cinemasci.viewers.version()
-        dbfile = os.path.join( destination, "cinema", viewer, version, "databases.json")
-
-        with open(dbfile, "w") as output:
-            json.dump(dbs, output, indent=4)
-
-    def __install_components(self):
-        compSrc  = os.path.join(__paths__["indir"], "components")
-        compDest = os.path.join(__paths__["cinema_out"], "components")
-        if not os.path.isdir(compDest):
-            print("Installing Components ...")
-            shutil.copytree( compSrc, compDest ) 
-
-    def __install_libs(self):
-        libsSrc  = os.path.join(__paths__["indir"], "cinema", "lib")
-        libsDest = os.path.join(__paths__["cinema_out"], "lib")
-        if not os.path.isdir(libsDest):
-            print("Installing libs ...")
-            shutil.copytree( libsSrc, libsDest ) 
+        return status
 
     #
     # copy the cinema directory to a destination folder
@@ -58,3 +61,29 @@ class install:
         source_viewer = os.path.join( cinemasci.path(), "viewers", "cinema_{}.html".format(viewer) )
         dest_viewer   = os.path.join( dest, "cinema_{}.html".format(viewer) )
         shutil.copyfile( source_viewer, dest_viewer )
+
+    #
+    # write a json-compliant db datset to a file
+    #
+    def __write_database_file(self, destination, viewer, dbs):
+        version = cinemasci.viewers.version()
+        dbfile = os.path.join( destination, "cinema", viewer, version, "databases.json")
+
+        with open(dbfile, "w") as output:
+            json.dump(dbs, output, indent=4)
+
+    def __install_components(self, destination):
+        compSrc  = os.path.join(destination, "components")
+        compDest = os.path.join(self.source, "components")
+        if not os.path.isdir(compDest):
+            print("Installing Components ...")
+            shutil.copytree( compSrc, compDest ) 
+
+    def __install_libs(self, destination):
+        libsSrc  = os.path.join(self.source, "viewers", "third_party") 
+        libsDest = os.path.join(destination, "cinema", "lib")
+        print("Installing libs ...")
+        print(libsSrc)
+        print(libsDest)
+        shutil.copytree( libsSrc, libsDest ) 
+
