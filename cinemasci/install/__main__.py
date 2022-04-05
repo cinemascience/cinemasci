00e41c4af81aa01d49fcb1ac02fa3e1c25828385
David Honegger Rogers <dhr@lanl.gov>
2022-04-05 17:45:39 -0600

    updating main and test plan

--- cinemasci/install/__main__.py
+++ cinemasci/install/__main__.py
@@ -5,11 +5,12 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="install a cinema viewer")
+    parser.add_argument("--path", required=True, default=None, help="destination for installation (required)") 
     parser.add_argument("--database", required=True, default=None, help="database to view (required)") 
     parser.add_argument("--type", default="remote", help="type of install (local, remote)") 
     parser.add_argument("--viewer", required=True, default='explorer', help="viewer type to use. One of [explorer, view] (required)") 
     args = parser.parse_args()
 
     installer = install()
-    abs_basepath = os.path.abspath(os.path.expanduser(args.database))
-    installer.install_viewer( os.path.dirname(abs_basepath), args.viewer, [os.path.basename(args.database)], args.type )
+    abs_path = os.path.abspath(os.path.expanduser(args.path))
+    installer.install_viewer( abs_path, args.viewer, [args.database], args.type )
